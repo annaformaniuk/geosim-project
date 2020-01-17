be4111d27e8e31cfd3843edd7a447ac1f633ad76
Juan Sebastian Garzón Alvarado <js.garzon398@uniandes.edu.co>
2020-01-17 13:01:36 +0100

    f.close()

--- model.py
+++ model.py
@@ -64,4 +64,5 @@ for percPrey in np.arange(0,1,propstepSize):
 
         f.write(str(float(percPrey)) + "," + str(float(percPred)) + "," + str(float(PercPreyFinal)) + "," + str(float(PercPredFinal))+"\n")
         print(str(float(percPrey)) + "," + str(float(percPred)) + "," + str(float(PercPreyFinal)) + "," + str(float(PercPredFinal))+"\n")
-
+             
+f.close()
